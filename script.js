8cb34f33534ba5128cae0b47575d64f9775397b9
Akash Srivastava <43779954+Dev-Akash@users.noreply.github.com>
2022-06-23 09:09:58 +0530

    Added drag and drop functionality

--- script.js
+++ script.js
@@ -3,3 +3,17 @@
 // var t1 = new Task("name", "desc", false);
 
 // console.log(t1);
+
+function drag(ev) {
+	console.log(ev);
+  ev.dataTransfer.setData("item", ev.target.id);
+}
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("item");
+  // console.log(ev);
+  ev.target.appendChild(document.getElementById(data));
+}
